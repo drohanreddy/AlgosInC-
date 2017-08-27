e89e11ba08cd00e4ff61dfb2cb9a4483b1bd0712
drohanreddy <drohanreddy@gmail.com>
2017-08-27 17:14:40 +0530

    Adding code files

---
+++ AlgoAllInOne/Algos/Array/TwoSum.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoAllInOne.Algos.Array
+{
+    public class TwoSum
+    {
+        private int _target;
+        private int[] _arr;
+        public TwoSum(int[] arr, int target)
+        {
+            _arr = arr;
+            _target = target;
+        }
+
+        /// <summary>
+        /// Basic method using two for loops. Not optimum
+        /// </summary>
+        /// <returns>An array of indices</returns>
+        public int[] basicMethod()
+        {
+            int[] output = new int[2];
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                for (int j = 1; j < _arr.Length; j++)
+                {
+                    if (_arr[i]+_arr[j] == _target && i != j)
+                    {
+                        if (i<j)
+                        {
+                            output[0] = i;
+                            output[1] = j;
+                        }
+                        else
+                        {
+                            output[0] = j;
+                            output[1] = i;
+                        }
+                       
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
---
+++ AlgoAllInOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using AlgoAllInOne.Algos.Array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoAllInOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoSum calculator = new TwoSum(new int[]{ 2, 3, 4, 5, 7, 1, 12, 15, 6 }, 20);
+            int[] output = calculator.basicMethod();
+            Console.WriteLine(output[0]);
+            Console.WriteLine(output[1]);
+        }
+    }
+}
