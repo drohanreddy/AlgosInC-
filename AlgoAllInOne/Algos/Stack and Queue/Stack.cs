146244d631615d6343020f0a8fef9ae70064c037
drohanreddy <drohanreddy@gmail.com>
2018-02-11 11:45:45 +0530

    o(1) minimum operation in stack

---
+++ AlgoAllInOne/Algos/Stack and Queue/Stack.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoAllInOne.Algos.Stack_and_Queue
+{
+    public class CustomStack
+    {
+        public StackNode top { get; set; }
+        public StackNode minimum { get; set; }
+        public CustomStack(int data)
+        {
+            StackNode n = new StackNode(data);
+            top = n;
+            minimum = n;
+        }
+
+        public void push(int data)
+        {
+            StackNode node = new StackNode(data);
+            if (top == null)
+            {
+                top = node;
+            }
+            else
+            {
+                if (data <= minimum.Data)
+                {
+                    var newMinimum = new StackNode(data);
+                    var oldMinimum = minimum;
+                    minimum = newMinimum;
+                    minimum.next = oldMinimum;
+
+                }
+                node.next = top;
+                top = node;
+                
+            }
+        }
+        public void pop()
+        {
+            if (top == null)
+            {
+                throw new Exception("Stack is empty");
+            }
+            else
+            {
+                Console.WriteLine("popped item is --"+top.Data);
+                if (top.Data == minimum.Data)
+                {
+                    minimum = minimum.next;
+                }
+                top = top.next;
+            }
+        }
+        public void GetMinimum()
+        {
+            if (top != null)
+            {
+                Console.WriteLine("minimum is "+minimum.Data);
+            }
+            
+        }
+        public void printStack()
+        {
+            Console.WriteLine("***Print Starts***");
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                var pointer = top;
+                while (pointer.next != null)
+                {
+                    Console.WriteLine(pointer.Data);
+                    pointer = pointer.next;
+                }
+                Console.WriteLine(pointer.Data);
+            }
+            Console.WriteLine("***Print Ends***");
+        }
+    }
+    public class StackNode
+    {
+        public int Data { get; set; }
+        public StackNode next { get; set; }
+        public StackNode(int data)
+        {
+            this.Data = data;
+            this.next = null;
+        }
+    }
+}
--- AlgoAllInOne/Program.cs
+++ AlgoAllInOne/Program.cs
@@ -1,6 +1,7 @@
 ﻿using AlgoAllInOne.Algos.Array;
 using AlgoAllInOne.Algos.Helper;
 using AlgoAllInOne.Algos.LinkedList;
+using AlgoAllInOne.Algos.Stack_and_Queue;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,10 +22,11 @@ namespace AlgoAllInOne
             // bool palindrome = Strings.palindromePermution("Tact Coa");
             //bool res = Strings.OneAway("pal", "pale");
             //string cmrs = Strings.Compress("aabbbbcdddd");
-#endregion 
-            var ll= trigger.LinkedList();
-            ll.print();
+            #endregion
+
             #region LinkedLists
+            //var ll= trigger.LinkedList();
+            //ll.print();
             #region remove duplicates
             //Wrapper.MethodCall<bool>(ll.removeDuplicatesOne);
             //ll.print();
@@ -34,7 +36,27 @@ namespace AlgoAllInOne
 
             // Last KTh Element Element
             //int lastEl = Wrapper.MethodCall<int,int>(ll.KThLastElement, 5);
-            trigger.SumList();
+            //  trigger.SumList();
+
+            // trigger.FindDuplicate();
+            #endregion
+
+            #region stack
+            CustomStack s = new CustomStack(10);
+            s.printStack();
+            s.GetMinimum();
+            s.push(9);
+            s.push(11);
+            s.push(14);
+            s.push(3);
+            s.GetMinimum();
+            s.pop();
+            s.pop();
+            s.push(3);
+            s.push(3);
+            s.GetMinimum();
+            s.pop();
+            s.printStack();
             #endregion
 
         }
@@ -83,6 +105,41 @@ namespace AlgoAllInOne
 
         }
 
+        public void FindDuplicate()
+        {
+            CLinkedList c = new CLinkedList();
+            c.Insert(4);
+            c.Insert(3);
+            c.Insert(1);
+            c.Insert(2);
+            c.Insert(4);
+            c.print();
+            var node = FindALoop(c);
+            if (node != c.GetRoot())
+            {
+                Console.WriteLine($"Faulty node is {node.Data}");
+            }
+            else
+            {
+                Console.WriteLine($"No faulty node in c");
+            }
+            CLinkedList c2 = new CLinkedList();
+            c2.Insert(5);
+            c2.Insert(1);
+            c2.Insert(3);
+            c2.Insert(4);
+            c2.print();
+            var node2 = FindALoop(c2);
+            if (node2 != c2.GetRoot())
+            {
+                Console.WriteLine($"Faulty node is {node2.Data}");
+            }
+            else
+            {
+                Console.WriteLine($"No faulty node in c2");
+            }
+        }
+
         private int LinkedListToNumber(CLinkedList c)
         {
 
@@ -91,17 +148,17 @@ namespace AlgoAllInOne
             int number = 0;
             while (true)
             {
-                if (start.Data !=0)
+                if (start.Data != 0)
                 {
                     int mutl = (int)Math.Pow(10, dig);
-                    number += (start.Data* mutl);
+                    number += (start.Data * mutl);
                 }
                 else
                 {
                     number += 0;
                 }
                 dig++;
-             
+
                 if (start.next == null)
                 {
                     break;
@@ -115,7 +172,7 @@ namespace AlgoAllInOne
         {
             CLinkedList c = new CLinkedList();
             int n = number;
-            while(n > 0)
+            while (n > 0)
             {
                 int x = n % 10;
                 c.Insert(x);
@@ -123,5 +180,45 @@ namespace AlgoAllInOne
             }
             return c;
         }
+
+        private Node FindALoop(CLinkedList cLinkedList)
+        {
+            var root = cLinkedList.GetRoot();
+            HashSet<int> hs = new HashSet<int>();
+            if (root != null)
+            {
+                var pointer = root;
+                while (pointer.next != null)
+                {
+                    if (found(ref hs, pointer.Data))
+                    {
+                        return pointer;
+                    }
+                    pointer = pointer.next;
+                }
+                if (found(ref hs, pointer.Data))
+                {
+                    return pointer;
+                }
+
+            }
+
+
+            return cLinkedList.GetRoot();
+        }
+
+        private bool found(ref HashSet<int> hs, int data)
+        {
+            if (hs.Contains(data))
+            {
+                return true;
+            }
+            else
+            {
+                hs.Add(data);
+                return false;
+            }
+        }
+
     }
 }
